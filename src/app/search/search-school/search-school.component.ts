23d55185ce10d57c49d4fbba293abae102c3af5f
Kryten <kryten2q4b@hotmail.co.uk>
2019-10-28 11:53:13 +0000

    added school and LA search skeletons

---
+++ src/app/search/search-school/search-school.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+//import { NgForm } from '@angular/forms';
+//import { PostsService } from '../posts.service';
+
+@Component({
+  selector : 'app-search-school',
+  templateUrl : './search-school.component.html',
+  styleUrls : ['./search-school.component.css']
+})
+export class SearchSchoolComponent {}
+ // enteredContent = '';
+  //enteredTitle = '' ;
+
+  //constructor(public postsService: PostsService) {}
+
+//   onAddPost(form: NgForm) {
+//     if (form.invalid) {
+//       return;
+//     }
+//     this.postsService.addPost(form.value.title, form.value.content);
+//     form.resetForm();
+//   }
