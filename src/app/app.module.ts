bcf0544644aede2b975ed2f3ac6e5534e687a66d
Kryten <kryten2q4b@hotmail.co.uk>
2019-10-30 10:38:34 +0000

    added search results page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SearchSchoolComponent } from './search/search-school/search-school.component';
 import { SearchLAComponent } from './search/search-la/search-la.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchSchoolComponent,
-    SearchLAComponent
+    SearchLAComponent,
+    SearchResultsComponent
   ],
   imports: [
     BrowserModule,
