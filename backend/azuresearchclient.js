121baf2736f18b45b56df3f9fe09c480e7987147
Christopher Morriss <Christopher.MORRISS@EDUCATION.GOV.UK>
2019-11-13 09:55:56 +0000

    added azure search client code

---
+++ backend/azuresearchclient.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+
+class AzureSearchClient {
+    constructor(searchServiceName, adminKey, queryKey, indexName) {
+        this.searchServiceName = searchServiceName;
+        this.adminKey = adminKey;
+        // The query key is used for read-only requests and so can be distributed with less risk of abuse.
+        this.queryKey = queryKey;
+        this.indexName = indexName;
+        this.apiVersion = '2019-05-06';
+    }
+
+    // All methods go inside class body here!
+}
+
+module.exports = AzureSearchClient;
